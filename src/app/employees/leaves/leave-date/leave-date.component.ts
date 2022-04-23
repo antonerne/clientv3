66feb33634a6bca164f6c2d45ca217b364b4dc6a
Anton P Erne <79037362+antonerne@users.noreply.github.com>
2022-04-23 19:07:46 +0900

    Apr23-1907

--- src/app/employees/leaves/leave-date/leave-date.component.ts
+++ src/app/employees/leaves/leave-date/leave-date.component.ts
@@ -8,23 +8,11 @@ import { LeaveGroup } from 'src/app/models/employee/leaves/leaveGroup';
   styleUrls: ['./leave-date.component.scss']
 })
 export class LeaveDateComponent implements OnInit {
-  private _leaveDates: LeaveGroup;
-  private _textColor: string = "000000";
-  private _isHoliday: boolean = false;
-  private _stdHours: number = 8;
 
-  @Input() set leavedates(value: LeaveGroup) {
-    this._leaveDates = value;
-  }
-  get leavedates(): LeaveGroup {
-    return this._leaveDates;
-  }
-  @Input() set textcolor(value: string) {
-    this._textColor = value;
-  }
-  get textcolor(): string {
-    return this._textColor;
-  }
+  @Input() leavedates: LeaveGroup = new LeaveGroup([]);
+  @Input() textcolor: string = "000000";
+  @Input() isholiday: boolean = false;
+  @Input() standardhours: number = 8.0;
 
   constructor() { }
 
@@ -34,11 +22,11 @@ export class LeaveDateComponent implements OnInit {
   getDisplay(): string {
     let months = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
       "Aug", "Sep", "Oct", "Nov", "Dec");
-    if (this._leaveDates.leaves.length > 1) {
+    if (this.leavedates.leaves.length > 1) {
       this.leavedates.leaves.sort((a,b) => a.compareTo(b));
       let date1: Date = this.leavedates.leaves[0].leave_date;
       let date2: Date = this.leavedates.leaves[this.leavedates.leaves.length - 1].leave_date;
-      if (this._isHoliday) {
+      if (this.isholiday) {
         return `${date1.getDate()} ${months[date1.getMonth()]} `
           + `- ${date2.getDate()} ${months[date2.getMonth()]}`;
       } else {
@@ -46,7 +34,7 @@ export class LeaveDateComponent implements OnInit {
       }
     } else {
       let date1: Date = this.leavedates.leaves[0].leave_date;
-      if (this._isHoliday) {
+      if (this.isholiday) {
         return `${date1.getDate()} ${months[date1.getMonth()]}`;
       } else {
         return `${date1.getDate()}`;
@@ -55,7 +43,7 @@ export class LeaveDateComponent implements OnInit {
   }
 
   getHours(): string {
-    if (this.leavedates.leaves[0].hours < this._stdHours) {
+    if (this.leavedates.leaves[0].hours < this.standardhours) {
       return this.leavedates.leaves[0].hours.toFixed(1);
     }
     return "";
